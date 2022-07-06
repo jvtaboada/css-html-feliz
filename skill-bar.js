2a5a740ab7ec893a0d9c129d196c4ed3e9caa54f
João Vitor <joao_taboada@hotmail.com>
2022-07-05 21:28:33 -0300

    Start skill-bar component

---
+++ skill-bar.js
@@ -0,0 +1,11 @@
+$(".skill-bar").each(function () {
+    $(this)
+      .find(".bar-inner")
+      .animate(
+        {
+          width: $(this).attr("data-width")
+        },
+        2000
+      );
+  });
+  
