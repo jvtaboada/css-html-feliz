f7b82211c8d9d4f2c74bb417795e32b2f626b3db
João Vitor <joao_taboada@hotmail.com>
2022-07-13 19:10:45 -0300

    Change let to const into objects

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let btnLeft = document.getElementById("btn-left");
-let btnRight = document.getElementById("btn-right");
-let carousel = document.getElementById("elements-carousel");
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right");
+const carousel = document.getElementById("elements-carousel");
 
 let pixels = 100;
 
 btnLeft.addEventListener('click', function leftTranslate(){
     pixels = pixels-100;
-    
+
     carousel.style = `transform: translateX(${pixels}px)`;
 });
 
